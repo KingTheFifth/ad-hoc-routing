cd73de5baaf8568af99579ba47bc6be2dc26fb44
David Larsson <davla442@student.liu.se>
2022-05-02 16:51:55 +0200

    Actual commit with changes staged this time

---
+++ simulator/host/routingTable.cpp
@@ -0,0 +1,37 @@
+#include "routingTable.h"
+
+vector<Host*>* RoutingTable::getRoute(Host *destination){
+    struct Row* tableEntry = getEntry(destination);
+    if (tableEntry == nullptr) { return nullptr; }
+    return &(tableEntry->route);
+}
+
+void RoutingTable::insert(Host* destination, vector<Host*> route, int cost, int sequenceNumber, int installTime){
+    //Create new row
+}
+
+void RoutingTable::insert(Host* destination, Host* route, int cost, int sequenceNumber, int installTime){
+    //Create route vector and call other insert function
+}
+
+void RoutingTable::remove(Host* destination){
+    for (vector<Row*>::iterator it = entries.begin(); it != entries.end(); it++){
+        if((*it)->destination == destination){
+            entries.erase(it);
+        }
+    }
+}
+void RoutingTable::update(RoutingTable* otherTable){
+    return;
+}
+
+Row* RoutingTable::getEntry(Host* host){
+    Row* entry = nullptr;
+    for (vector<Row*>::iterator it = entries.begin(); it != entries.end(); it++){
+        if((*it)->destination == host){
+            entry = *it;
+            break;
+        }
+    }
+    return entry;
+}
--- simulator/host/routingTable.h
+++ simulator/host/routingTable.h
@@ -1,4 +1,31 @@
 #ifndef ROUTINGTABLE_H
 #define ROUTINGTABLE_H
 
+#include <vector>
+
+using namespace std;
+
+class Host;
+
+struct Row {
+    Host* destination;
+    vector<Host*> route; //For DSDV the route is just a single node (next-hop)
+    int cost;
+    int sequenceNumber;
+    int installTime;
+};
+
+class RoutingTable{
+    public:
+        vector <Host*>* getRoute(Host* destination);
+        void insert(Host* destination, vector<Host*> route, int cost = 0, int sequenceNumber = 0, int installTime = 0);
+        void insert(Host* destination, Host* route, int cost = 0, int sequenceNumber = 0, int installTime = 0);
+        void remove(Host* destination);
+        void update(RoutingTable* otherTable); //updates local routing table from other table
+    private:
+        Row* getEntry(Host* host);
+        vector<Row*> entries;
+};
+
+
 #endif // ROUTINGTABLE_H
